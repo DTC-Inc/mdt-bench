64aa140aab3b99471f1a6131459f071508fefc75
Nate Smith <40123869+nettts@users.noreply.github.com>
2023-01-23 21:21:41 -0500

    Update filesystem-config.ps1

--- servers/filesystem-config.ps1
+++ servers/filesystem-config.ps1
@@ -1,3 +1,1 @@
 New-Item -path D:\ -name 'repo' -itemtype directory
-New-Item -path D:\ -name 'Virtual Machines' -itemtype directory
-New-Item -path D:\ -name 'Virtual Hard Disks' -itemtype directory
