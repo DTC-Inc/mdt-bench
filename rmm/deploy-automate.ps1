254f05edf45a4ff2297cc37cbbfc8082b04e3688
Nate Smith <40123869+nettts@users.noreply.github.com>
2023-02-15 14:15:17 -0500

    Update deploy-automate.ps1

--- rmm/deploy-automate.ps1
+++ rmm/deploy-automate.ps1
@@ -26,6 +26,32 @@ $Password = $null
 #Set LocationID to install the Automate Agent directly to the appropieate client's location / site.
 $LocationId = Read-Host "Enter location ID"
 
+
+# Prompt the user for input on whether to install on one or many computers
+$installOption = Read-Host "Do you want to install the application on one or many computers? (Type 1 for one, 2 for many)"
+
+if ($installOption -eq "1") {
+    # Install on one computer
+    $computerName = Read-Host "Enter the name of the computer you want to install the application on"
+    # Replace this with your actual application installation command
+    # Example: Start-Process "setup.exe" -ArgumentList "/silent" -Wait
+    Write-Host "Installing application on $computerName..."
+} elseif ($installOption -eq "2") {
+    # Install on many computers
+    $searchOU = Read-Host "Enter the Active Directory OU path to search for computers"
+    $computerNames = Get-ADComputer -SearchBase $searchOU -Filter * | Select-Object -ExpandProperty Name
+    foreach ($computerName in $computerNames) {
+        # Replace this with your actual application installation command
+        # Example: Invoke-Command -ComputerName $computerName -ScriptBlock { Start-Process "setup.exe" -ArgumentList "/silent" -Wait }
+        Write-Host "Installing application on $computerName..."
+    }
+} else {
+    Write-Host "Invalid input. Please enter 1 or 2."
+}
+
+
+
+
 ##### Script Logic #####
 
 if(($Token -eq $null) -and  ($Password -eq $null)) {
