6c6cbb62491a28cd07e80bbaa37008911d4ab9a8
Nate Smith <40123869+nettts@users.noreply.github.com>
2023-11-17 14:50:48 -0500

    Update deploy-apps.ps1

--- servers/deploy-apps.ps1
+++ servers/deploy-apps.ps1
@@ -2,7 +2,7 @@
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-$AppList = 'firefox','7zip','vscode','vcredist-all','adobereader' 
+$AppList = 'firefox','7zip','vscode','vcredist-all'
 
 $AppList | ForEach-Object -Process {choco install $_ -y}
 $AppList | ForEach-Object -Process {choco upgrade $_ -y}
